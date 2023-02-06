33c9d9bf551cafbfeb84500a095eab5aacef9084
Michael Bayouk <mb49491@uga.edu>
2023-02-06 13:55:02 -0500

    updated deploy

--- michaelbayo.uk/deploy.sh
+++ michaelbayo.uk/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Switching to master branch"
-git checkout master
+echo "Switching to main branch"
+git checkout main
 
 echo "Building app"
 npm run build
 
 echo "Deploying files"
 rsync -avP build/ -e "ssh -p 22" root@michaelbayo.uk:/var/www/michaelbayo.uk/
-echo "Complete"
+echo "Completed Deployment"
